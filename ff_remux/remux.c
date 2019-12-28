63ffc011797873e8f63798a6ab7583d2562ea0f2
xiyuyizhi <1553810509@qq.com>
2019-12-28 20:34:13 +0800

    chore

---
+++ ff_remux/remux.c
@@ -0,0 +1,33 @@
+
+#include "libavutil/timestamp.h"
+#include "structs.h"
+#include "base.h"
+
+typedef struct _State
+{
+
+} State;
+
+static void read_packet()
+{
+}
+
+static void seek_packet()
+{
+}
+
+static void write_packet()
+{
+}
+
+int do_remux()
+{
+}
+
+int process()
+{
+}
+
+int main()
+{
+}
